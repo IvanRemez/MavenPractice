c036d6388609110c4dcfe8d1f1aa00cabdf65f77
Ivan_Cydeo <remezivan1@gmail.com>
2023-02-16 09:18:29 -0500

    T2

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,6 @@
+import java.util.Set;
+
+public abstract class MyClass {
+
+    public abstract Set<Character> myMethod(char[] ch);
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test extends MyClass{
+
+
+    @Override
+    public Set<Character> myMethod(char[] ch) {
+        Set<Character> result = new HashSet<>();
+        for (int i = 0; i < ch.length; i++) {
+            result.add(ch[i]);
+        }
+        return result;
+    }
+}
